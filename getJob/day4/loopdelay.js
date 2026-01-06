3555142cdbb77afdb724cd2e86dd8a2c9048f102
rohit@ROHIT <rohitsingh95724@gmail.com>
2026-01-07 01:52:02 +0530

    added

--- getJob/day4/loopdelay.js
+++ getJob/day4/loopdelay.js
@@ -5,7 +5,22 @@ function loopDelay(ms){
     }
 
 }
-
 console.log("Start");
 loopDelay(2000);  // 2 sec delay
 console.log("End after 2 sec");
+
+
+console.log("start");
+
+setTimeout(() => {
+  console.log("hello world");
+}, 3000);
+
+const startTime = Date.now();
+let endTime = startTime;
+
+while (endTime < startTime + 10000) { // 10 seconds block
+  endTime = Date.now();
+}
+
+console.log("end");
